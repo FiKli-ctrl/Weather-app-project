52dfb97f3b66a06ca1ff1258bcb38e3a5716d6ea
FiKli-ctrl <fionaklimecki1@gmail.com>
2025-02-05 12:53:32 +0100

    Added forecast display JS

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ function updateWeather(response){
     let iconElement = document.querySelector("#icon");
     
 
-    console.log(response.data);
-
 iconElement.innerHTML = `<img
     src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
     cityElement.innerHTML = response.data.city;
@@ -48,7 +46,33 @@ function handleSearchSubmit(event){
     searchCity(searchInput.value);
 }
 
+function displayForecast() {
+   
+    let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let forecastHtml = "";
+
+    days.forEach(function (day) {
+        forecastHtml =
+            forecastHtml + 
+        `
+        <div class="weather-forecast-day">
+                  <div class="weather-forecast-date">${day}</div>
+                  <div class="weather-forecast-icon">⛅</div>
+                  <div class="weather-forecast-temperatures">
+                    <div class="weather-forecast-temperature"><strong>6°C</strong></div>
+                    <div class="weather-forecast-temperature">-1°C</div>  
+                  </div>
+                </div> 
+                `;
+    });
+    
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;  
+}
+   
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Amsterdam");
+displayForecast();
