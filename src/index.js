bb0ed830ed373839908651fd1ccda30ca454379b
FiKli-ctrl <fionaklimecki1@gmail.com>
2025-02-05 14:32:09 +0100

    Integrate API Forecast

--- src/index.js
+++ src/index.js
@@ -48,6 +48,13 @@ function handleSearchSubmit(event){
     searchCity(searchInput.value);
 }
 
+function formatForecastDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+    return days[date.getDay()];
+}
+
 function getForecast(city) {
     let apiKey = "b9d4b0f1302d1ta5ba14cob9718ff6e7";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -58,22 +65,25 @@ function displayForecast(response) {
    
     console.log(response.data);
 
-    let days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     let forecastHtml = "";
 
-    days.forEach(function (day) {
+    response.data.daily.forEach(function (day, index) {
+        if (index < 6 && index > 0) {
         forecastHtml =
             forecastHtml + 
         `
         <div class="weather-forecast-day">
-                  <div class="weather-forecast-date">${day}</div>
-                  <div class="weather-forecast-icon">⛅</div>
+                  <div class="weather-forecast-date">${formatForecastDay(day.time)}</div>
+                  <div >
+                  <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+                  </div>
                   <div class="weather-forecast-temperatures">
-                    <div class="weather-forecast-temperature"><strong>6°C</strong></div>
-                    <div class="weather-forecast-temperature">-1°C</div>  
+                    <div class="weather-forecast-temperature"><strong>${Math.round(day.temperature.maximum)}°C</strong></div>
+                    <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°C</div>  
                   </div>
                 </div> 
                 `;
+        }
     });
     
     let forecastElement = document.querySelector("#forecast");
